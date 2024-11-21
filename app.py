f3b8b77ac64b59298b387169e9a6d22d2991fe76
Oz174 <ahmedtarek1754@gmail.com>
2024-11-21 09:53:39 +0200

    improvements

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+from flask import Flask, request, jsonify
 from joblib import load
 
-loaded_m = load('model.pkl')
-loaded_s = load('scaler.pkl')
-loaded_features = load('features.pkl')
+app = Flask(__name__)
+
+loaded_m = load('output/model.pkl')
+loaded_s = load('output/scaler.pkl')
+loaded_features = load('output/features.pkl')
 loaded_features = list(loaded_features)
 
-
+@app.route('/predict', methods=['POST'])
 def predict(device_json : dict) -> int : 
   """
   Takes in the json/dict of the device and return predcition

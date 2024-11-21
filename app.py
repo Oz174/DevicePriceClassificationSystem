20b85ddd52a8cfa152f2ec86227f7861af8b26de
Ahmed Tarek (オリ ー オ) <83515041+Oz174@users.noreply.github.com>
2024-11-21 09:27:21 +0200

    Python File + Binary Files

---
+++ app.py
@@ -0,0 +1,32 @@
+from joblib import load
+
+loaded_m = load('model.pkl')
+loaded_s = load('scaler.pkl')
+loaded_features = load('features.pkl')
+loaded_features = list(loaded_features)
+
+
+def predict(device_json : dict) -> int : 
+  """
+  Takes in the json/dict of the device and return predcition
+  
+  Params:  
+  device json : dictionairy of the input device features 
+  
+  Return:
+  int : prediction of device price range
+  """
+  # drop id
+  _ = device_json.pop('id')
+
+  # scale 
+  scaled_device_input = loaded_s.transform([list(device_json.values())])
+  # reconsturct the dict 
+  device_json = dict(zip(list(device_json.keys()),scaled_device_input[0]))
+
+  # filter out unwanted features
+  device_json = {k:v for k,v in device_json.items() if k in loaded_features}
+  # prediction phase
+  prediction = loaded_m.predict([list(device_json.values())])
+
+  return int(prediction)
